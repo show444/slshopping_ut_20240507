55627f7248a5defa27ff932c9308099617e64c29
show444 <156166487+show444@users.noreply.github.com>
2024-05-07 16:04:17 +0900

    chapter2Lesson3まで完了

--- src/test/java/com/example/slshopping_ut/brand/BrandServiceTest.java
+++ src/test/java/com/example/slshopping_ut/brand/BrandServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.slshopping_ut.brand;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 
 import com.example.slshopping_ut.entity.Brand;
 
@@ -48,8 +50,80 @@ class BrandServiceTest {
         // スタブの設定
         doReturn(expected).when(this.mockBrandRepository).findAll();
 
-        /* Lesson02 タスク -初級編- 課題1 */
         // 検証処理
+        assertThat(target.listAll()).isEqualTo(expected);
+    }
+
+    /**
+     * 【概要】
+     * ブランドを検索<br>
+     *
+     * 【条件】
+     * brandServiceのlistAllメソッドにnullを渡すこと<br>
+     * brandRepositoryのfindAllメソッドはBrandのリストを返却するようスタブ化すること<br>
+     *
+     * 【結果】
+     * Brandのリストを返却すること
+     */
+    @Test
+    void testListAll_argumentIsNull() {
+        List<Brand> expected = Arrays.asList(
+            new Brand(1L, "brandA"),
+            new Brand(2L, "brandB")
+        );
+        
+        doReturn(expected).when(this.mockBrandRepository).findAll();
+        //target.listAll(null)を修正
+        assertThat(target.listAll()).isEqualTo(expected);
+    }
+
+    /**
+     * 【概要】
+     * 商品を検索<br>
+     *
+     * 【条件】
+     * brandServiceのlistAllメソッドに空文字を渡すこと<br>
+     * brandRepositoryのfindAllメソッドはBrandのリストを返却するようスタブ化すること<br>
+     *
+     * 【結果】
+     * Brandのリストを返却すること
+     */
+    @Test
+    void testListAll_argumentIsEmpty() {
+        List<Brand> expected = Arrays.asList(
+            new Brand(1L, "brandA"),
+            new Brand(2L, "brandB")
+        );
+
+        doReturn(expected).when(this.mockBrandRepository).findAll();
+        //BrandServiceのif分で修正
+        assertThat(target.listAll("")).isEqualTo(expected);
+    }
+
+    /**
+     * 【概要】
+     * ブランドを検索<br>
+     *
+     * 【条件】
+     * brandServiceのlistAllメソッドにbrandという文字列を渡すこと<br>
+     * brandRepositoryのsearchメソッドはBrandのリストを返却するようスタブ化すること<br>
+     *
+     * 【結果】
+     * Brandのリストを返却すること
+     */
+    @Test
+    void testListAll_argumentIsNotEmpty() {
+        //このテストははひとつ前のテストの修正で同時に成功になった。
+        String keyword = "brand";
+
+        List<Brand> expected = Arrays.asList(
+            new Brand(1L, "brandA"),
+            new Brand(2L, "brandB")
+        );
+
+        doReturn(expected).when(this.mockBrandRepository).search(keyword);
+
+        assertThat(target.listAll(keyword)).isEqualTo(expected);
     }
 
     /**
@@ -70,8 +144,60 @@ class BrandServiceTest {
         // スタブの設定
         doReturn(null).when(this.mockBrandRepository).findByName(anyString());
 
-        /* Lesson02 タスク -初級編- 課題2 */
         // 検証処理
+        assertThat(target.checkUnique(newBrand)).isTrue();
+    }
+
+    /**
+     * 【概要】
+     * ブランド名の重複チェック<br>
+     *
+     * 【条件】
+     * brandRepositoryのfindByNameメソッドはBrandのインスタンスを返却するようスタブ化すること<br>
+     *
+     * 【結果】
+     * falseを返すこと
+     */
+    @Test
+    void testCheckUnique_duplicate() {
+        // 準備 ブランド名が重複するブランド情報を作成
+        Brand newBrand = new Brand(1L, "brandA");
+
+        // スタブに設定するデータを作成
+        Brand mockBrand = new Brand(1L, "brandA");
+
+        //スタブの設定
+        doReturn(mockBrand).when(this.mockBrandRepository).findByName(newBrand.getName());
+
+        // 検証
+        assertThat(target.checkUnique(newBrand)).isFalse();
+    }
+
+    /**
+     * 【概要】
+     * ブランド情報の取得<br>
+     *
+     * 【条件】
+     * brandRepositoryのfindByIdメソッドはBrandのインスタンスを格納したOptionalを返却するようスタブ化すること<br>
+     *
+     * 【結果】
+     * 例外が発生しないこと
+     */
+    @Test
+    void testGet_noThrowsException() {
+        // 準備 テストデータに存在するID
+        Long id = 1L;
+
+        // スタブに設定するデータを作成
+        Optional<Brand> brand = Optional.of(new Brand());
+
+        //スタブの設定
+        doReturn(brand).when(this.mockBrandRepository).findById(id);
+
+        // 検証
+        assertThatCode(() -> {
+            target.get(id);
+        }).doesNotThrowAnyException();
     }
 
     /**
@@ -82,7 +208,7 @@ class BrandServiceTest {
      * brandRepositoryのfindByIdメソッドはnullを格納したOptionalを返却するようスタブ化すること<br>
      *
      * 【結果】
-     * 例外が発生すること
+     * 例外が発生しないこと
      */
     @Test
     void testGet_throwsException() {
@@ -95,7 +221,35 @@ class BrandServiceTest {
         //スタブの設定
         doReturn(brand).when(this.mockBrandRepository).findById(id);
 
-        /* Lesson02 タスク -初級編- 課題3 */
         // 検証処理
+        assertThatThrownBy(() -> {
+            target.get(id);
+        })
+        .isInstanceOf(NotFoundException.class);
+    }
+
+    /**
+     * 【概要】
+     * ブランド情報の取得処理の検証<br>
+     *
+     * 【条件】
+     * brandRepositoryのfindByIdはBrandのインスタンスを格納したOptionalを返却するようスタブ化すること<br>
+     *
+     * 【結果】
+     * Brandを返却すること
+     */
+    @Test
+    void testGet() throws Exception {
+        // 準備 任意のID
+        Long id = 1L;
+
+        // スタブに設定するデータを作成
+        Optional<Brand> brand = Optional.of(new Brand());
+
+        // スタブの設定
+        doReturn(brand).when(this.mockBrandRepository).findById(id);
+
+        // 検証
+        assertThat(target.get(id)).isEqualTo(brand.get());
     }
 }
